70c086ce170669b7254fc3454360a5024ed77030
Kenichi TAKAHASHI <kenichi.taka@gmail.com>
2010-12-01 19:40:27 +0900

    fav command

--- lingr.rb
+++ lingr.rb
@@ -1,3 +1,4 @@
+require 'rss'
 set :views, File.join(File.dirname(__FILE__), 'views')
 
 get '/' do
@@ -8,12 +9,16 @@ post '/' do
   parser = Yajl::Parser.new
   hash = parser.parse(params[:json])
   hash["events"].map do |event|
-    if event["message"]["text"] =~ /^!ruby (.+)$/
+    case event["message"]["text"]
+    when /^!ruby (.+)$/
       begin
         eval $1
       rescue Exception => e
         e.message
       end
+    when /^!fav$/
+      item = RSS::Parser.new('http://favstar.fm/users/htkymtks/rss').parse.items.choice
+      "#{item.title}\n#{item.link}"
     else
       nil
     end
