dc1dd5be03e082aabc2304f5a739c5b83fa93959
Keita Urashima <ursm@ursm.jp>
2010-12-01 20:03:40 +0900

    !ruby= command

--- lingr.rb
+++ lingr.rb
@@ -1,4 +1,6 @@
 require 'rss'
+require 'stringio'
+
 set :views, File.join(File.dirname(__FILE__), 'views')
 
 get '/' do
@@ -15,6 +17,16 @@ post '/' do
         eval $1
       rescue Exception => e
         e.message
+      end
+    when /^!ruby= (.+)$/
+      begin
+        buf = $stdout = $stderr = StringIO.new
+        eval $1
+        buf.string
+      rescue Exception => e
+        e.message
+      ensure
+        $stdout, $stderr = STDOUT, STDERR
       end
     when /^!fav$/
       item = RSS::Parser.new('http://favstar.fm/users/htkymtks/rss').parse.items.choice
